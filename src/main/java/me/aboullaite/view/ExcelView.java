c651594042527a301d4f3bfb7c3aae0849f1b165
red1 <redouaneelalami@gmail.com>
2019-03-19 10:23:53 +0100

    espace

--- src/main/java/me/aboullaite/view/ExcelView.java
+++ src/main/java/me/aboullaite/view/ExcelView.java
@@ -38,7 +38,7 @@ public class ExcelView extends AbstractXlsView{
         font.setColor(HSSFColor.WHITE.index);
         style.setFont(font);
 
-
+        
         // create header row
         Row header = sheet.createRow(0);
         header.createCell(0).setCellValue("FirstName");
